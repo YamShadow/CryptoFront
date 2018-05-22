ca893b49d10bdc0a2ed30dfbd30f5df6dfbf2491
dylanol <oliveira.dylann@gmail.com>
2018-05-22 16:57:59 +0200

    Tableau crypto-monnaies

--- js/script.js
+++ js/script.js
@@ -1,15 +1,43 @@
 window.onload = function() {
     $.ajax({
         type: 'GET',
-        url: 'http://yamishadow.fr/Crypto/API/cryptocurrencies',
+        url: 'http://yamishadow.fr/Crypto/cryptocurrencies',
         data: {},
         dataType: 'json',
         success: function(data) {
-            $(data).each(function(i, val) {
-                $.each(val, function(k, v) {
-                    $("#tab-cryptos").append('<tr><td>' + k + '</td><td>' + v + '</td></tr>');
+
+            $(function() {
+                $.each(data, function(i, item) {
+                    var $tr = $('<tr>').append(
+                        $('<td>').text(item.name),
+                        $('<td>').text(item.symbol),
+                        $('<td>').text(item.rank)
+                    ); //.appendTo('#records_table');
+                    $("#tab-cryptos").append($tr);
                 });
             });
+
         }
     });
 };
+
+
+$(function() {
+    $('#action').click(function() {
+        $.ajax({
+            type: 'GET',
+            url: 'http://yamishadow.fr/Crypto/cryptocurrencies/symbol/' + $('input[name="input"]').val(),
+            data: {},
+            dataType: 'json',
+            success: function(data) {
+                $(data).each(function(i, val) {
+                    $.each(val, function(champ, valeur) {
+                        if (champ == 'id') { return true; }
+
+                        $("#tab-cryptos").append('<tr><td>' + champ + '</td><td>' + valeur + '</td></tr>');
+                    });
+                });
+            }
+        });
+    });
+});
